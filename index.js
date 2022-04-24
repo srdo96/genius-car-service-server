57eee970be2e84d70949b56338e2446421d61ac8
Sakib Rahman <seu.sakib@gmail.com>
2022-04-24 19:55:53 +0600

    add DELETE

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ async function run() {
       const result = await serviceCollection.insertOne(newService);
       res.send(result);
     });
+
+    // DELETE
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
